8703eaf2edfaaa31901564db9c195c1356636502
GIHAA <gihansudeeparandimal@gmail.com>
2023-12-26 21:38:30 +0530

    Fix: fix cors

--- Backend/src/main/java/com/demo/proclientmanager/controller/HealthController.java
+++ Backend/src/main/java/com/demo/proclientmanager/controller/HealthController.java
@@ -4,6 +4,7 @@ import com.demo.proclientmanager.payload.common.ResponseEntityDto;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
