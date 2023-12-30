462e9f13ffb21cc344b71db79acd701e3327ebab
GIHAA <gihansudeeparandimal@gmail.com>
2023-12-30 07:46:21 +0530

    Fix: fe table ui changes

--- Frontend/src/pages/Home.tsx
+++ Frontend/src/pages/Home.tsx
@@ -140,7 +140,7 @@ const Home = () => {
       <Sidebar />
       <div className=" md:ml-64 h-auto ml-[75px] ">
         <div className=" rounded-lg  dark:border-gray-600 h-screen ">
-          <section className="bg-gray-50 dark:bg-gray-900 h-screen p-3 sm:p-5">
+          <section className="bg-gray-50 dark:bg-gray-900 md:h-auto h-auto lg:h-screen p-3 sm:p-5">
             <div className=" mx-auto max-w-screen-xl px-4 lg:px-2  pt-[50px]">
               <div className="bg-white mt-[10px] dark:bg-gray-800 relative shadow-md sm:rounded-lg overflow-hidden">
                 <div className="flex flex-col md:flex-row items-center justify-between space-y-3 md:space-y-0 md:space-x-4 p-4">
@@ -229,7 +229,7 @@ const Home = () => {
                   </>
                 ) : (
                   <>
-                    <div className="max-h-[520px] h-screen overflow-y-auto">
+                    <div className="max-h-[490px] h-screen overflow-y-auto">
                       <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
                         <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                           <tr>
